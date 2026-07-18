09fb032952df08c63ee6206a967d9799940a7dc2
uqasimm <muhammadqasim2k07@gmail.com>
2026-07-18 23:22:24 +0500

    Problem #03 Solved

---
+++ Patterns/Question-3.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    printPattern(n);
+    
+    return 0;
+}
