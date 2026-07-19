91c3d85df23faec8b5a28c2136fe4a40b3e778b7
uqasimm <muhammadqasim2k07@gmail.com>
2026-07-19 21:49:09 +0500

    Problem #11 Solved

---
+++ Patterns/Question-11.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if ((i + j) % 2 == 0)
+            {
+                cout << "1 ";
+            }
+            else
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    printPattern(n);
+    
+    return 0;
+}
