04f08af635bcccf9d808872fb7756c549a160676
uqasimm <muhammadqasim2k07@gmail.com>
2026-07-23 09:43:32 +0500

    Queue Complete, Priority_Queue Started

---
+++ STL/priority_queue.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainPriority_Queue(){
+
+}
+
+int main(){
+    explainPriority_Queue();
+    return 0;
+}
--- STL/queue.cpp
+++ STL/queue.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// FIFO (First In First Out)
+
 void explainQueue(){
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.emplace(4);
+
+    q.back() += 5;
+    cout << q.back() << endl;
+
+    queue<int> temp = q;
+    while (!temp.empty())
+    {
+        cout << temp.front() << " ";
+        temp.pop();
+    }
     
 }
 
