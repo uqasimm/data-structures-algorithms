9fa7348187e2f00f919b3cf2f7fb9bb056ecdd43
uqasimm <muhammadqasim2k07@gmail.com>
2026-07-23 20:40:52 +0500

    Set Complete, MultiSet Started

---
+++ STL/multiSet.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainMultiSet(){
+
+}
+
+int main(){
+    explainMultiSet();
+    return 0;
+}
--- STL/set.cpp
+++ STL/set.cpp
@@ -1,8 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void explainSet(){
+// Set stores in a SORTED ORDER and UNIQUE values
 
+// Operations take O(log N), because set is stored in a form of Red-Black Tree, if we use unordered_set then it doesnt sort the elements, it just store unique values and its faster than simple "set" as it takes average O(1) constant time
+
+void explainSet(){
+    set<int> s;
+    s.insert(1);
+    s.emplace(2);
+    s.insert(2); // it doesnt store 2 again
+    s.insert(4);
+    s.insert(5);
+    s.insert(3);
+
+    auto it = s.find(4); // if number is not found it refers to s.end()
+
+    s.erase(3);
+
+    for (auto i : s)
+    {
+        cout << i << " ";
+    }
+    
+    cout << endl;
+
+    cout << s.count(3) << endl; // count gives either 1 (if found) or 0 (not found)
+
+    auto it_erase1 = s.find(2);
+    auto it_erase2 = s.find(4);
+    s.erase(it_erase1, it_erase2); // 2, 3 will be deleted
+    
+    for (auto i : s)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    auto bound1 = s.lower_bound(4); // find first index of 4 or greater than 4
+    cout << *(bound1) << endl;
+
+    auto bound2 = s.upper_bound(4); // find first index of greater than 4
+    cout << *(bound2) << endl;
 }
 
 int main(){
