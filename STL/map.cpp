a07cd648eda7d2632c50d74a10f626cc62a6c2c4
uqasimm <muhammadqasim2k07@gmail.com>
2026-07-24 09:45:18 +0500

    UnorderedSet Complete, Map Started

---
+++ STL/map.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explainMap(){
+
+}
+
+int main(){
+    explainMap();   
+    return 0;
+}
--- STL/unordered_set.cpp
+++ STL/unordered_set.cpp
@@ -1,11 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void explainUnordered_Set(){
-    
+// it stores UNIQUE but in NO ORDER / RANDOM ORDER
+
+// no lower and upper bound functions
+
+// O(1) for the operations, but RARELY WORST CASE: Time Complexity goes to O(N)
+
+void explainUnordered_Set()
+{
+    unordered_set<int> ust;
+
+    ust.insert(2);
+    ust.insert(2);
+    ust.insert(5);
+    ust.insert(10);
+    ust.insert(8);
+    ust.insert(7);
+
+    // bucket is just a container used in hash table to make diff operations fast
+
+    cout << ust.bucket(8) << endl;  // bucket index in which 10 is stored
+    cout << ust.bucket_count() << endl; // total number of buckets
+    cout << ust.load_factor() << endl; // average number of elements in a bucket
+
+    for (auto i : ust)
+    {
+        cout << i << " ";
+    }
 }
 
-int main(){
+int main()
+{
     explainUnordered_Set();
     return 0;
 }
